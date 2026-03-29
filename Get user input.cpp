dbc280fd4eb295cadf983441da8cf2ef1d47db39
yasmin-coder-1510 <yasminakter20031@gmail.com>
2026-03-29 16:02:28 +0600

    Add files via upload

---
+++ Get user input.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+   int num1;
+
+   cout<<"Enter an integer number : " ;
+   cin>>num1 ;
+   cout<<"Entered number : "<<num1 <<endl;
+
+   double num2;
+
+   cout<<"Enter an double number : " ;
+   cin>>num2 ;
+   cout<<"Entered number : "<<num2 <<endl ;
+
+   char ch;
+   cout<<"Enter a letter : " ;
+   cin>>ch ;
+   cout<<"Entered letter : "<<ch <<endl;
+
+
+  char name[13] ;
+
+  cout<<"Enter your name :"<<name;
+  cin>>name ;
+  cout<<"Your name is :"<<name ;
+
+ getch();
+}
+
